7f1c51a21aed61dd1aaead6cc60d668f4badc0ef
Rudiansyah <rudd.rocker@gmail.com>
2022-11-20 13:17:36 +0800

    add gitignore

--- index.py
+++ index.py
@@ -3,6 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return '<h1>Hello World!!</h1>'
 
-app.add_url_rule('/', 'index', index)
+# app.add_url_rule('/', 'index', index)
+
+# @app.route('/user/<name>')
+# def user(name):
+#     return '<h1>Hello, {}!</h1>'.format(name)
